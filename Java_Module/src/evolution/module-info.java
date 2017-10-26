cccd20caca1891890d866c0b0b1a8839b812bb14
chenli <chenli@192.168.43.24>
2017-10-26 10:28:24 +0800

    initial commit

---
+++ Java_Module/src/evolution/module-info.java
@@ -0,0 +1,8 @@
+module evolution {
+	// The following are all modules.
+    requires java.se;
+    requires java.sql;
+    requires java.base;
+    requires java.compiler;
+    requires jdk.jlink;
+}
